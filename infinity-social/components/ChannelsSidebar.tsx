16e25674360fcec25613f6b20d0c703734ec5b41
BiswadeepInfinity <141620203+BiswadeepInfinity@users.noreply.github.com>
2026-08-28 21:59:38 +0530

    fix: import TrendingUp icon in ChannelsSidebar

--- infinity-social/components/ChannelsSidebar.tsx
+++ infinity-social/components/ChannelsSidebar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useChannelsStore } from '@/lib/channels-store';
 import { 
-  Flame, 
+  TrendingUp,
   Compass, 
   Plus, 
   Gamepad2, 
